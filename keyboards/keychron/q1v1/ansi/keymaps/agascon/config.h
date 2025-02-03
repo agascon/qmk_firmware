3acaea9f3558933dbefd1fa5f267586b1e644b8d
Antonio Gascón <3040982+agascon@users.noreply.github.com>
2025-02-03 09:36:26 +0100

    Fix for issue while Mac wake up

--- keyboards/keychron/q1v1/ansi/keymaps/agascon/config.h
+++ keyboards/keychron/q1v1/ansi/keymaps/agascon/config.h
@@ -3,6 +3,8 @@
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
 
+#define USB_SUSPEND_WAKEUP_DELAY 200
+
 /* RGB Matrix Configuration */
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED
